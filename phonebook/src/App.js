d774e63d142d4d0deccd78e39bbac201c02ea95d
daniElPapu <daniel.bedergal@tecsup.edu.pe>
2021-04-16 22:32:48 -0500

    PhoneBoook- Se agregó phone y lo correspondiente para agregar y mostrar

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,21 +2,25 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone:'123456789' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone] = useState('')
 
   const submit=(e)=>{
     e.preventDefault()
     let person = persons.find(person => person.name === newName);
-    if(person !== undefined){
+    if(person !== undefined ){
       alert(`${newName} is already added to phonebook`)
     }
-    else{
+    else if(newName != '' && newPhone != ''){
       setPersons([
         ...persons,
-        {name: newName}
+        {name: newName,phone:newPhone}
       ])
+    }
+    else{
+      alert("Debe de completar todos los campos")
     }
     setNewName('')
   }
@@ -28,20 +32,23 @@ const App = () => {
           name: <input value={newName} onChange={(e)=>setNewName(e.target.value)}/>
         </div>
         <div>
+          phone: <input  value={newPhone} onChange={(e)=>setNewPhone(e.target.value)}/>
+        </div>
+        <div>
           <button onClick={submit} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person)=>{
         return (
-          <Person key={person.name} name={person.name}/>
+          <Person key={person.name} name={person.name} phone={person.phone}/>
         )
       })}
     </div>
   )
 }
-const Person=({name})=>{
-  return <> {name}<br/></>
+const Person=({name,phone})=>{
+  return <> {name} {phone}<br/></>
 }
 
 export default App
