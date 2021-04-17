ad6b8d34a56ebaefee0abf949563e7b05ea8a369
daniElPapu <daniel.bedergal@tecsup.edu.pe>
2021-04-16 22:49:57 -0500

    PhoneBoook- Se realizó una refactorización en componentes más simples para mejorar el entendimiento y mantenimiento del código

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -23,32 +23,54 @@ const App = () => {
       alert("Debe de completar todos los campos")
     }
     setNewName('')
+    setNewPhone('')
   }
   return (
     <div>
       <h2>Phonebook</h2>
-        <form>
-          filter shown with <input value={filter} onChange={(e)=>setFilter(e.target.value)}/>
-        </form>
+      <Filter filter={filter} setFilter={setFilter}/>
       <h2>add a new</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={(e)=>setNewName(e.target.value)}/>
-        </div>
-        <div>
-          phone: <input  value={newPhone} onChange={(e)=>setNewPhone(e.target.value)}/>
-        </div>
-        <div>
-          <button onClick={submit} type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm 
+        newName={newName}
+        newPhone={newPhone}
+        setNewName={setNewName}
+        setNewPhone={setNewPhone}
+        submit={submit}
+      />
       <h2>Numbers</h2>
-      {persons.filter(person=>person.name.includes(filter)).map((person)=>{
-        return (
-          <Person key={person.name} name={person.name} phone={person.phone}/>
-        )
-      })}
+      <Persons persons={persons} filter={filter}/>
     </div>
+  )
+}
+const Filter =({filter,setFilter})=>{
+  return (
+    <form>
+      filter shown with <input value={filter} onChange={(e)=>setFilter(e.target.value)}/>
+    </form>
+  )
+}
+const PersonForm = ({newName,newPhone,setNewName,setNewPhone,submit})=>{
+  return (
+    <form>
+      <div>
+        name: <input value={newName} onChange={(e)=>setNewName(e.target.value)}/>
+      </div>
+      <div>
+        phone: <input  value={newPhone} onChange={(e)=>setNewPhone(e.target.value)}/>
+      </div>
+      <div>
+        <button onClick={submit} type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+const Persons =({persons,filter})=>{
+  return (<>
+    {persons.filter(person=>person.name.includes(filter)).map((person)=>{
+      return (
+        <Person key={person.name} name={person.name} phone={person.phone}/>
+      )
+    })}</>
   )
 }
 const Person=({name,phone})=>{
